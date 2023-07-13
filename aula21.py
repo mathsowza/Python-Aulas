50721e90dfa34328ed3ea55aa00a31f2ebf6c142
MatheusSouzaCS <65633721+mathsowza@users.noreply.github.com>
2023-07-13 10:34:11 -0300

    Update aula21.py

--- aula21.py
+++ aula21.py
@@ -3,4 +3,15 @@
 # and - Todas as condições precisam ser verdadeiras. Se qualquer valor for considerado falso, a expressão inteira será avaliada naquele valor.
 # São condiderados falsy: 0, 0.0, '', False
 # Também existe o tipo None que é usado para representar um não-valor
-#
+
+# entrada = input('[E]entrar [S]air: ')
+# senha_digitada = input('Senha: ')
+
+# senha_permitida = '123456'
+# if entrada == 'E' and senha_digitada == senha_permitida:
+#     print('Entrar')
+# else:
+#     print('Sair')
+
+# Avaliação de curto-circuito
+print(False and True and True)
